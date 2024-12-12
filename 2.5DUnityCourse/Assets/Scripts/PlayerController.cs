46815bbadfc71372f37781a966636d63e744f7ac
Lauren <lntaylor.me@gmail.com>
2024-12-12 12:09:38 -0500

    Created Player Controller Script

---
+++ 2.5DUnityCourse/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = playerControls.Player.Move.ReadValue<Vector2>().x;
+        float z = playerControls.Player.Move.ReadValue<Vector2>().y;
+        
+        Debug.Log(x + "," + z);
+    }
+}
