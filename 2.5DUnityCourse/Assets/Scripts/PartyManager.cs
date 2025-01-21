507388da00e875fc759f2829132f125825efa1d3
Lauren <lntaylor.me@gmail.com>
2025-01-21 14:48:49 -0500

    Created Party Manager Script

---
+++ 2.5DUnityCourse/Assets/Scripts/PartyManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyManager : MonoBehaviour
+{
+    [SerializeField] private PartyMemberInfo[] allMembers;
+    [SerializeField] private List<PartyMember> currentParty;
+
+    [SerializeField] private PartyMemberInfo defaultPartyMember;
+
+    private void Awake(){
+        AddMemberToPartyByName(defaultPartyMember.MemberName);
+    }
+
+    public void AddMemberToPartyByName(string memberName)
+    {
+        for (int i = 0; i < allMembers.Length; i++)
+        {
+            if (allMembers[i].MemberName == memberName)
+            {
+                PartyMember newPartyMember = new PartyMember();
+                newPartyMember.MemberName = allMembers[i].MemberName;
+                newPartyMember.Level = allMembers[i].StartingLevel;
+                newPartyMember.CurrHealth = allMembers[i].BaseHealth;
+                newPartyMember.MaxHealth = newPartyMember.CurrHealth;
+                newPartyMember.Strength = allMembers[i].BaseStrength;
+                newPartyMember.Speed = allMembers[i].BaseSpeed;
+                newPartyMember.MemberBattleVisualPrefab = allMembers[i].MemberBattleVisualPrefab;
+                newPartyMember.MemberOverworldVisualPrefab = allMembers[i].MemberOverworldVisualPrefab;
+
+                currentParty.Add(newPartyMember);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class PartyMember
+{
+    public string MemberName;
+    public int Level;
+    public int CurrHealth;
+    public int MaxHealth;
+    public int Strength;
+    public int Speed;
+    public int CurrExp;
+    public int MaxExp;
+    public GameObject MemberBattleVisualPrefab;
+    public GameObject MemberOverworldVisualPrefab;
+}
